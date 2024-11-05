1a54d55d12fb3b79092d0fc3f8536478deb80384
mdmahfuz307 <njnirob2019@gmail.com>
2024-11-05 17:33:48 +0600

    Get Request with URL Query

--- 04-Request/03-Get Request with URL Query/index.js
+++ 04-Request/03-Get Request with URL Query/index.js
@@ -2,7 +2,13 @@ let express = require('express');
 
 app = express();
 
+app.get("/",function(req,res){
 
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+
+    res.end(firstName + " " + lastName);
+});
 
 // http://localhost:8000?firstName=Nirob&lastName=Mahfuz (ai ta use kore post man e get request debo)
 
