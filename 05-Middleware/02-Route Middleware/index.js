8c1fe7b0c9ec1cd8c14ca9fe85fd0d87431a2150
mdmahfuz307 <njnirob2019@gmail.com>
2024-11-05 21:13:28 +0600

    Route Middleware

--- 05-Middleware/02-Route Middleware/index.js
+++ 05-Middleware/02-Route Middleware/index.js
@@ -3,8 +3,28 @@ let app = express();
 
 
 app.get('/', function(req, res) {
-    res.send('Hello, World!');
+    res.send("This is Home page");
 });
+
+
+app.get('/contact', function(req, res) {
+    res.send("This is contact page");
+});
+
+
+app.use('/about',function(req,res,next){
+    
+    console.log("I am about middleware");
+    next();
+
+});
+
+
+app.get('/about', function(req, res) {
+    res.send("This is about page");
+});
+
+
 
 app.listen(8000, function() {
     console.log('Server running on port 8000');
